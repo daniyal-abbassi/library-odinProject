8d64b670c721996a08f5e98e255744471d2bbeeb
daniyal-abbassi <slimshady3753@protonmail.com>
2025-05-28 13:45:19 +0330

    setup script skeletone

---
+++ script.js
@@ -0,0 +1,11 @@
+//get DOM elements
+
+//array to store books
+
+//Book Object Constructore
+
+//add methods for creating and storing books to array with specific id
+
+//buttons to add and delete and edit read status for books
+
+//loop throght books and render them on page
