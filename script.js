3ccbd0ffe247d4346fb6e2e59a04c5c521514a52
daniyal-abbassi <slimshady3753@protonmail.com>
2025-05-28 13:56:52 +0330

    setup constructor test

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 //get DOM elements
+const bookList = document.getElementById('books');
 
 //array to store books
+let booksArray = [];
 
 //Book Object Constructore
+function Book(name,author) {
+    this.name = name;
+    this.author = author;
+}
+Book.prototype.sayName = function() {
+    console.log(`the Author of ${this.name} is ${this.author} .`)
+}
+const book1 = new Book('novel','Erick');
 
 //add methods for creating and storing books to array with specific id
 
