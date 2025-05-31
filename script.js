0cf14a186fc612f8e4f4726596f5caae226c33c3
daniyal-abbassi <slimshady3753@protonmail.com>
2025-05-31 19:56:49 +0330

    chore: add creating book function

--- script.js
+++ script.js
@@ -5,17 +5,27 @@ const bookList = document.getElementById('books');
 let booksArray = [];
 
 //Book Object Constructore
-function Book(name,author) {
+function Book(name,author,id) {
     this.name = name;
     this.author = author;
+    this.id = id;
 }
-Book.prototype.sayName = function() {
-    console.log(`the Author of ${this.name} is ${this.author} .`)
+Book.prototype.sayBook = function() {
+   console.log(`book name: ${this.name}, and author is: ${this.author}`)
 }
-const book1 = new Book('novel','Erick');
 
-//add methods for creating and storing books to array with specific id
 
+//add a function for creating and storing books to array with specific id
+const createAndSave = (name,author) => {
+    //random new id
+    let newId = crypto.randomUUID();
+    //create with Object constructor
+    let newBook = new Book(name,author,newId);
+    //add to array
+    booksArray.push(newBook);
+}
+
+// booksArray.map(book=>console.log(book.name,book.author,book.id))
 //buttons to add and delete and edit read status for books
 
 //loop throght books and render them on page
